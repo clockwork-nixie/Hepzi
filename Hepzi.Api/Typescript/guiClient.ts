f7ff8d792a8eca40389c5d99bb6520bdfa000ff4
Nikki <Nikki.Barnett@tracker.co.uk>
2021-12-26 22:35:46 +0000

    Made canvas correct size.

--- Hepzi.Api/Typescript/guiClient.ts
+++ Hepzi.Api/Typescript/guiClient.ts
@@ -8,6 +8,33 @@ namespace Hepzi {
             if (canvas && canvas instanceof HTMLCanvasElement) {
                 console.log("YAY");
                 var engine = new BABYLON.Engine(canvas as HTMLCanvasElement, true, { preserveDrawingBuffer: true, stencil: true });
+
+                var createScene = function () {
+                    var scene = new BABYLON.Scene(engine);
+                    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+
+                    camera.setTarget(BABYLON.Vector3.Zero());
+                    camera.attachControl(canvas, false);
+
+                    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+                    var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE);
+
+                    sphere.position.y = 1;
+
+                    var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
+
+                    return scene;
+                }
+
+                var scene = createScene();
+
+                engine.runRenderLoop(function () {
+                    scene.render();
+                });
+
+                window.addEventListener('resize', function () {
+                    engine.resize();
+                });
             }
         }
     }
